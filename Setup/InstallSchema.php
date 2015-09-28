69777adcb57b55d4e0f3f02b5cfad3e7217d3c73
Tym17 <tym17@users.noreply.github.com>
2015-09-28 16:48:11 +0200

    Test config

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tym17\MailPerformance\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+
+        $installer->startSetup();
+
+        /**
+        * Create table 'mailperf_targets'
+        */
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('mailperf_targets')
+        )->addColumn(
+            'id_magento',
+            \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+            11,
+            array('identity' => true, 'nullable' => false, 'primary' => true),
+            'magento ID'
+        )->addColumn(
+            'id_mailperf',
+            \Magento\Framework\DB\Ddl\Table::TYPE_VARCHAR,
+            8,
+            array('nullable' => false),
+            'mperf id'
+        )->setComment(
+        'MailPerformance Table'
+        );
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
+
+    }
+}
