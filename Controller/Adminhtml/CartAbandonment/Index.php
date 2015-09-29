da40356273d41a10d9b79b4e3ec680b3a600641a
Tym17 <tym17@users.noreply.github.com>
2015-09-29 18:26:25 +0200

    Page prepare

---
+++ Controller/Adminhtml/CartAbandonment/Index.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tym17\MailPerformance\Adminhtml\CartAbandonment;
+
+class Index extends \MAgento\Backend\App\Action
+{
+  /**
+  * Index Action*
+  * @return void
+  */
+  public function execute()
+  {
+    die('hello world!')
+  }
+}
---
+++ Controller/Adminhtml/CustomerManagement/Index.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tym17\MailPerformance\Adminhtml\CustomerManagement;
+
+class Index extends \MAgento\Backend\App\Action
+{
+  /**
+  * Index Action*
+  * @return void
+  */
+  public function execute()
+  {
+    die('hello world!')
+  }
+}
