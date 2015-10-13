7fb5ddcb6c1ce1beda6c9178cb0ac890c67f03eb
Tym17 <tym17@users.noreply.github.com>
2015-10-13 15:22:00 +0200

    Revert "jquery not working"
    This reverts commit e2121f40134a2e341d74cacfdf1af6318a8ea527.

--- Block/Adminhtml/Custom/Tab.php
+++ Block/Adminhtml/Custom/Tab.php
@@ -6,26 +6,6 @@ use Magento\Backend\Block\Template;
 class Tab extends Template
 {
     /**
-     * @var string
-     */
-    public $navTitle = null;
-
-    /**
-     * @var array
-     */
-    public $tabName;
-
-    /**
-     * @var array
-     */
-    public $tabContent;
-
-    /**
-     * @var integer
-     */
-    public $tabAmmount = 0;
-
-    /**
     * @param Context $context
     * @param array $data
     */
@@ -34,29 +14,5 @@ class Tab extends Template
         array $data = []
     ) {
         parent::__construct($context, $data);
-    }
-
-    public function getTabId()
-    {
-        return $this->_nameInLayout;
-    }
-
-    /**
-     * @param string
-     */
-    public function setNavTitle($title)
-    {
-        $this->navTitle = $title;
-    }
-
-    /**
-     * @param string
-     * @param string
-     */
-    public function addTab($name, $content)
-    {
-        $this->tabName[] = array('name' => $name, 'content' => $content);
-        $this->tabContent[] = $content;
-        $this->tabAmmount += 1;
     }
 }
